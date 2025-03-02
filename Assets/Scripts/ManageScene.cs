391b3c81bd8a467a79a93cae87fa87e1469f4a7d
Iván <ivan.leris@enti.cat>
2025-03-02 13:12:33 +0100

    UI in game

--- Assets/Scripts/ManageScene.cs
+++ Assets/Scripts/ManageScene.cs
@@ -1,13 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ManageScene : MonoBehaviour
 {
     //private List<Transform> semafaros = new List<Transform>();
     public int numCarsToLose = 16;
+    public int numCarsToStar;
     private int numCars = 0;
     private int numCarsDestroyed = 0;
+
+    //Timer
+    private float startTime;  
+    private float elapsedTime;
+
+    //Quejas
+    private int quejas;
+
+    public TextMeshProUGUI timerText;
+    public TextMeshProUGUI crashed_text;
+    public TextMeshProUGUI quejas_txt;
 
     public SoundManager soundManager;
 
@@ -15,6 +28,8 @@ public class ManageScene : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        startTime = Time.time;
+
         foreach (Transform child in transform)
         {
             Spawneador_coches spw = child.GetComponent<Spawneador_coches>();
@@ -28,9 +43,26 @@ public class ManageScene : MonoBehaviour
         if (destroyed) numCarsDestroyed++;
     }
 
+    public void Quejarse()
+    {
+        quejas++;
+    }
+
     // Update is called once per frame
     void Update()
     {
+        if (!finish)
+        {
+            elapsedTime = Time.time - startTime;
+
+            if (timerText != null)
+            {
+                timerText.text = elapsedTime.ToString("F2") + " s";
+                crashed_text.text = numCarsDestroyed.ToString();
+                quejas_txt.text = quejas.ToString();
+            }
+        }
+
         if (numCars == 0 && !finish)
         {
             //WIN
